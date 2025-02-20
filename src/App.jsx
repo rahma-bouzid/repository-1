c30cee8ea3840645cf44ead9ef0d5736a7a84d3b
rahma bouzid <rahma04.bouzid@gmail.com>
2025-02-20 17:24:45 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Test commit to force changes
 
 import * as React from "react";
 function getTitle(title){
